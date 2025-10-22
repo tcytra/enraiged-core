6bfd6a706fab04cd4c27c3488d05fbef0a435fe4
Todd Cytra <tcytra@gmail.com>
2025-10-22 09:47:54 -0700

    Update: Updated Users UserResource to provide more robust information

--- src/Users/Resources/UserResource.php
+++ src/Users/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace Enraiged\Users\Resources;
 
+use Enraiged\Avatars\Resources\AvatarResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,12 +19,31 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $resource = [
             'id' => $this->id,
+            'active' => $this->isActive,
+            'alias' => $this->profile->alias,
             'email' => $this->email,
             'locale' => $this->locale,
             'name' => $this->name,
+            'phone' => $this->profile->phone,
+            'salut' => $this->profile->salut,
+            'theme' => $this->theme,
             'username' => $this->username,
+            'avatar' => (new AvatarResource($this->profile->avatar)),
+            'created' => $this->created,
+            'deleted' => !is_null($this->deleted_at)
+                ? $this->deleted
+                : false,
+            'updated' => !is_null($this->updated_at)
+                ? $this->updated
+                : false,
         ];
+
+        if ($this->isProtected) {
+            $resource['protected'] = true;
+        }
+
+        return $resource;
     }
 }
